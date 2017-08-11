480e1ef8cd90a6bd3b7a8cba0793d9ec8dc3b659
KrunalLathiya <krunallathiya21@gmail.com>
2017-08-12 04:49:56 +0530

    NodeJS Email Example Complete

---
+++ server.js
@@ -0,0 +1,43 @@
+var express = require('express'),
+    path = require('path'),
+    nodeMailer = require('nodemailer'),
+    bodyParser = require('body-parser');
+
+    var app = express();
+    app.set('view engine', 'ejs');
+    app.use(express.static('public'));
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
+    var port = 3000;
+    app.get('/', function (req, res) {
+      res.render('index');
+    });
+    app.post('/send-email', function (req, res) {
+      let transporter = nodeMailer.createTransport({
+          host: 'smtp.gmail.com',
+          port: 465,
+          secure: true,
+          auth: {
+              user: 'xxx@xx.com',
+              pass: 'xxxx'
+          }
+      });
+      let mailOptions = {
+          from: '"Krunal Lathiya" <xx@gmail.com>', // sender address
+          to: req.body.to, // list of receivers
+          subject: req.body.subject, // Subject line
+          text: req.body.body, // plain text body
+          html: '<b>NodeJS Email Tutorial</b>' // html body
+      };
+
+      transporter.sendMail(mailOptions, (error, info) => {
+          if (error) {
+              return console.log(error);
+          }
+          console.log('Message %s sent: %s', info.messageId, info.response);
+              res.render('index');
+          });
+      });
+          app.listen(port, function(){
+            console.log('Server is running at port: ',port);
+          });
